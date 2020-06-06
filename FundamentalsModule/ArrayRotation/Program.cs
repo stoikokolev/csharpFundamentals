8c68c4395563f34735cb123896e0909ac7d4fc46
Стойко Kolev <stoikokolev@gmail.com>
2020-06-06 11:56:55 +0300

    array excercise

---
+++ FundamentalsModule/ArrayRotation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+            if (number>arr.Length)
+            { number = number%arr.Length; }
+            for (int i = number; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+        }
+    }
+}
---
+++ FundamentalsModule/TopIntegers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int biggest = int.MinValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                biggest = int.MinValue;
+                for (int n = i+1; n < arr.Length; n++)
+                {
+                   if (arr[n]>biggest)
+                    {
+                        biggest = arr[n];
+                    }
+                }
+                if (arr[i]>biggest)
+                { 
+                    Console.Write(arr[i]+" "); 
+                }
+                
+            }
+        }
+    }
+}
