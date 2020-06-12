6aa81080b93687c7e8bc19ebcf2644fb3ce23bd2
Стойко Kolev <stoikokolev@gmail.com>
2020-06-12 20:11:51 +0300

    Methods Excercise

---
+++ FundamentalsModule/AddAndSubtract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Subtract(Add(num1, num2), num3));
+        }
+
+        private static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        private static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
