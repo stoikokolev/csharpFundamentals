103704251a1d4cca1ab98a1d509d5e9e4b72a70e
Стойко Kolev <stoikokolev@gmail.com>
2020-06-19 19:04:35 +0300

    Lists Excercise

---
+++ FundamentalsModule/ChangeList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] command = input
+                    .Split();
+
+                if (command[0] == "Delete")
+                {
+                    Delete(list, int.Parse(command[1]));
+                }
+                else if (command[0] == "Insert")
+                {
+                    Insert(list, int.Parse(command[1]), int.Parse(command[2]));
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', list));
+
+        }
+
+        private static void Insert(List<int> list, int element, int index)
+        {
+            list.Insert(index, element);
+        }
+
+        private static void Delete(List<int> list, int element)
+        {
+            while (list.Remove(element))
+            {
+
+            }
+        }
+    }
+}
---
+++ FundamentalsModule/TrainLists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrainLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagon = new List<int>();
+            wagon = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int maxCapacity = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] command = input.Split().ToArray();
+                if (command[0] == "Add")
+                {
+                    wagon.Add(int.Parse(command[1]));
+                }
+                else
+                {
+                    for (int i = 0; i < wagon.Count; i++)
+                    {
+                        if (int.Parse(command[0]) + wagon[i] <= maxCapacity)
+                        {
+                            wagon[i] += int.Parse(command[0]);
+                            break;
+                        }
+
+                    }
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', wagon));
+
+
+
+        }
+    }
+}
