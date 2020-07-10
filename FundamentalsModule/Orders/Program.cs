85e3dd4b73c81cd7ed1b3b059da737cc171f124b
Стойко Kolev <stoikokolev@gmail.com>
2020-07-10 22:37:55 +0300

    Associative Arrays Exercise

---
+++ FundamentalsModule/Orders/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var menu = new Dictionary<string, Product>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "buy")
+                {
+                    break;
+                }
+                string name = input.Split()[0];
+                double price = double.Parse(input.Split()[1]);
+                int quantity = int.Parse(input.Split()[2]);
+                var product = new Product(name, price, quantity);
+                if (menu.ContainsKey(name))
+                {
+                    menu[name].Price = price;
+                    menu[name].Quantity += quantity;
+                }
+                else
+                {
+                    menu.Add(name, product);
+                }
+
+            }
+            foreach (var item in menu)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value.Price * item.Value.Quantity:f2}");
+            }
+        }
+
+        public class Product
+        {
+            public string Name;
+            public double Price;
+            public int Quantity;
+            public Product(string name, double price, int quantity)
+            {
+                this.Name = name;
+                this.Price = price;
+                this.Quantity = quantity;
+            }
+        }
+
+    }
+}
---
+++ FundamentalsModule/SoftUniParking/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeats = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, Ticket>();
+            for (int i = 0; i < repeats; i++)
+            {
+                var input = Console.ReadLine().Split();
+                string command = input[0];
+                string name = input[1];
+                switch (command)
+                {
+                    case "register":
+                        string plate = input[2];
+                        if (dict.ContainsKey(name))
+                        {
+                            Console.WriteLine($"ERROR: already registered with plate number {dict[name].plate}");
+                        }
+                        else
+                        {
+                            dict.Add(name, new Ticket(name, plate));
+                            Console.WriteLine($"{name} registered {plate} successfully");
+                        }
+                        break;
+
+                    case "unregister":
+                        if (dict.ContainsKey(name))
+                        {
+                            Console.WriteLine($"{name} unregistered successfully");
+                            dict.Remove(name);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ERROR: user {name} not found");
+                        }
+                        break;
+
+                }
+
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value.plate}");
+            }
+
+        }
+        public class Ticket
+        {
+            public string name { get; set; }
+            public string plate { get; set; }
+
+            public Ticket(string name, string plate)
+            {
+                this.name = name;
+                this.plate = plate;
+            }
+        }
+    }
+}
