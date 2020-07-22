745e0d6aeade6220ec70a5b4c1f664050db7ca7d
Стойко Kolev <stoikokolev@gmail.com>
2020-07-22 18:25:57 +0300

    Text Processing Exercise

---
+++ FundamentalsModule/MultiplyBigNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            input = input.TrimStart(new char[] { '0' });
+            int multyplier = int.Parse(Console.ReadLine());
+            int remainder = 0;
+            int current = 0;
+            List<int> list = new List<int>();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                remainder = (input[i] - '0') * multyplier + remainder;
+                current = remainder % 10;
+                remainder /= 10;
+                list.Add(current);
+
+            }
+            if (remainder > 0)
+            {
+                list.Add(remainder);
+            }
+            list.Reverse();
+            string final = string.Join("", list);
+            final = final.TrimStart(new char[] { '0' });
+            if (final == string.Empty)
+            {
+                final = "0";
+            }
+            Console.WriteLine(final);
+
+            
+        }
+    }
+}
---
+++ FundamentalsModule/StringExplosion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int power = 0;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i] == '>')
+                {
+                    power += input[i + 1] - '0';
+                    sb.Append(input[i].ToString());
+                }
+                else if (power > 0)
+                {
+                    power--;
+                }
+                else
+                {
+                    sb.Append(input[i].ToString());
+                }
+
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
