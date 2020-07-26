d551ea17df1bbe75cd8653c2d15f0c33d88937e4
Стойко Kolev <stoikokolev@gmail.com>
2020-07-27 00:16:48 +0300

    Regular Expressions Exercise

---
+++ FundamentalsModule/Furniture/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @">>(?<name>[A-z]+)<<(?<price>\d+\.?\d+?)!(?<quantity>\d+)";
+            double sum = 0;
+            List<string> list = new List<string>();
+            while (input != "Purchase")
+            {
+                var match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    list.Add(match.Groups["name"].Value);
+                    sum += double.Parse(match.Groups["price"].Value) * double.Parse(match.Groups["quantity"].Value);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Total money spend: {sum:f2}");
+        }
+    }
+}
---
+++ FundamentalsModule/Race/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[A-Za-z]";
+            string pattern2 = @"\d";
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            string[] players = Console.ReadLine().Split(", ").ToArray();
+            foreach (var player in players)
+            {
+                if (!dict.ContainsKey(player))
+                {
+                    dict.Add(player, 0);
+                }
+            }
+            string input = Console.ReadLine();
+
+            while (input != "end of race")
+            {
+                var matches = Regex.Matches(input, pattern);
+                var matches2 = Regex.Matches(input, pattern2);
+                string name = string.Empty;
+
+                foreach (var match in matches)
+                {
+                    name += match.ToString();
+                }
+                if (dict.ContainsKey(name))
+                {
+                    foreach (var number in matches2)
+                    {
+                        dict[name] += int.Parse(number.ToString());
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            int count = 1;
+            foreach (var item in dict.OrderByDescending(x => x.Value).Take(3))
+            {
+                if (count == 1)
+                {
+                    Console.WriteLine($"{count}st place: {item.Key}");
+                }
+                else if (count ==2)
+                {
+                    Console.WriteLine($"{count}nd place: {item.Key}");
+                }
+                else
+                {
+                    Console.WriteLine($"{count}rd place: {item.Key}");
+                }
+                
+                count++;
+            }
+
+
+
+        }
+    }
+}
