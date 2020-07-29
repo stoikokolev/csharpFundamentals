454ca29602e8a35b9b9a75d058e8f8f7d286fc6a
Stoyko Kolev <stoikokolev@gmail.com>
2020-07-29 19:42:43 +0300

    Final Exams Exercise

---
+++ FundamentalsModule/FancyBarcodes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FancyBarcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeats = int.Parse(Console.ReadLine());
+            string pattern = @"@#+([A-Z][A-Za-z0-9]{4,}[A-Z])@#+";
+            for (int i = 0; i < repeats; i++)
+            {
+                string barcode = Console.ReadLine();
+                var match = Regex.Match(barcode, pattern);
+                if (match.Success)
+                {
+                    barcode = match.Groups["1"].Value;
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < barcode.Length; j++)
+                    {
+                        if (char.IsDigit(barcode[j]))
+                        {
+                            sb.Append(barcode[j].ToString());
+                        }
+                    }
+
+                    if (sb.ToString()==string.Empty)
+                    {
+                        sb.Append("00");
+                    }
+                    Console.WriteLine($"Product group: {sb.ToString()}");
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ FundamentalsModule/PasswordReset/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace PasswordReset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string command = Console.ReadLine();
+            while (command != "Done")
+            {
+                string[] arr = command.Split();
+                switch (arr[0])
+                {
+                    case "TakeOdd":
+                        string newInput = string.Empty;
+                        for (int i = 0; i < input.Length; i++)
+                        {
+                            if (i % 2 == 1)
+                            {
+                                newInput += input[i];
+                            }
+
+                        }
+
+                        input = newInput;
+                        Console.WriteLine(input);
+                        break;
+                    case "Cut":
+                        input = input.Remove(int.Parse(arr[1]), int.Parse(arr[2]));
+                        Console.WriteLine(input);
+                        break;
+                    case "Substitute":
+                        if (input.IndexOf(arr[1]) >= 0)
+                        {
+                            input = input.Replace(arr[1], arr[2]);
+                            Console.WriteLine(input);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+
+                        
+                        break;
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your password is: {input}");
+        }
+    }
+}
