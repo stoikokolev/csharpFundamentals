ec1f6738e5ce0ea44332541bc125f1d39d50cb08
Stoyko Kolev <stoikokolev@gmail.com>
2020-08-03 21:39:55 +0300

    Final Exam April 2019

---
+++ FundamentalsModule/ArrivingInKathmandu/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ArrivingInKathmandu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^(?<name>[A-Za-z\d!@#$?]+)=(?<lenght>\d+)<<(?<code>.+)$";
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "Last note")
+            {
+                var match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    if (int.Parse(match.Groups["lenght"].Value) == match.Groups["code"].Length)
+                    {
+                        var sb = new StringBuilder();
+                        string name = match.Groups["name"].Value;
+                        for (int i = 0; i < name.Length; i++)
+                        {
+                            if (char.IsLetterOrDigit(name[i]))
+                            {
+                                sb.Append(name[i]);
+                            }
+
+                        }
+
+                        Console.WriteLine($"Coordinates found! {sb} -> {match.Groups["code"].Value}");
+                        continue;
+                    }
+
+
+                }
+
+
+                Console.WriteLine("Nothing found!");
+            }
+
+        }
+    }
+}
---
+++ FundamentalsModule/OnTheWayToAnnapurna/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnTheWayToAnnapurna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            while ((input=Console.ReadLine())!="END")
+            {
+                string[] command = input.Split("->", StringSplitOptions.RemoveEmptyEntries);
+                if (command[0]=="Add")
+                {
+                    string[] items = command[2].Split(',', StringSplitOptions.RemoveEmptyEntries);
+                    if (dict.ContainsKey(command[1]))
+                    {
+                        foreach (var item in items)
+                        {
+                            if (!dict[command[1]].Contains(item))
+                            {
+                                dict[command[1]].Add(item);
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        dict.Add(command[1], new List<string>());
+                        dict[command[1]].Add(command[2]);
+                    }
+
+                }
+                else if (command[0]=="Remove")
+                {
+                    if (dict.ContainsKey(command[1]))
+                    {
+                        dict.Remove(command[1]);
+                    }
+
+                }
+
+            }
+            Console.WriteLine("Stores list:");
+            foreach (var item in dict.OrderByDescending(x=>x.Value.Count).ThenByDescending(x=>x.Key))
+            {
+                Console.WriteLine(item.Key);
+                foreach (var thing in item.Value)
+                {
+                    Console.WriteLine($"<<{thing}>>");
+                }
+            }
+
+        }
+    }
+}
+    
