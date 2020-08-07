4aa6e47aaa7429c272ac4d633dc83864e2ba4fbc
Stoyko Kolev <stoikokolev@gmail.com>
2020-08-07 11:51:33 +0300

    Final Exam August 2019

---
+++ FundamentalsModule/MessageDecrypter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MessageDecrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^([$%])(?<tag>[A-Z][a-z]{2,})\1: \[(?<first>[0-9]+)\]\|\[(?<second>[0-9]+)\]\|\[(?<third>[0-9]+)\]\|$";
+
+            int repeats = int.Parse(Console.ReadLine());
+            for (int i = 0; i < repeats; i++)
+            {
+                string input = Console.ReadLine();
+                var match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    string text = match.Groups["tag"].Value + ": "
+                        + (char)(int.Parse(match.Groups["first"].Value))
+                        + (char)(int.Parse(match.Groups["second"].Value)) 
+                        + (char)(int.Parse(match.Groups["third"].Value));
+                    Console.WriteLine(text);
+                }
+                else
+                {
+                    Console.WriteLine("Valid message not found!");
+                }
+            }
+
+        }
+    }
+}
---
+++ FundamentalsModule/StringManipulator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace StringManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            string text = Console.ReadLine();
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] command = input.Split();
+                if (command.Contains("Translate"))
+                {
+                    text = text.Replace((command[1]), (command[2]));
+                    Console.WriteLine(text);
+                }
+                else if (command.Contains("Includes"))
+                {
+                    if (text.Contains(command[1]))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (command.Contains("Start"))
+                {
+                    if (text.IndexOf(command[1]) == 0)
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else if (command.Contains("Lowercase"))
+                {
+                    text = text.ToLower();
+                    Console.WriteLine(text);
+                }
+                else if (command.Contains("FindIndex"))
+                {
+                    Console.WriteLine(text.LastIndexOf((command[1])));
+                }
+                else if (command.Contains("Remove"))
+                {
+                    text = text.Remove(int.Parse(command[1]), int.Parse(command[2]));
+                    Console.WriteLine(text);
+                }
+            }
+        }
+    }
+}
